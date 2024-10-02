9878205f33478dce8ef8cb01ce0881eccbc6913b
Alex Petrenko <alex.pt@ya.ru>
2024-10-02 11:38:56 +0300

    refactoring

--- TicTacToe/Screens/SelectGame/SelectGame.swift
+++ TicTacToe/Screens/SelectGame/SelectGame.swift
@@ -8,15 +8,17 @@
 import SwiftUI
 
 struct SelectGame: View {
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
         NavigationView {
             ZStack {
+                Constants.Colors.background
                 Rectangle()
                     .frame(width: 285, height: 247)
                     .cornerRadius(30)
                     .foregroundColor(.white)
-                    .shadow(radius: 10)
-                
+                    .shadow(color: Color(red: 0.6, green: 0.62, blue: 0.76).opacity(0.3), radius: 15, x: 4, y: 4)
                 VStack {
                     Text("Select Game")
                         .font(.system(size: 24))
@@ -26,6 +28,16 @@ struct SelectGame: View {
                     SelectGameButtton(image: Constants.Icons.singlePlayer, text: "Single Player")
                     SelectGameButtton(image: Constants.Icons.twoPlayers, text: "Two Players", isSelected: false)
                         .padding()
+                }
+            }
+            .ignoresSafeArea()
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }, label: {
+                        Constants.Icons.back
+                    })
                 }
             }
             .toolbar {
