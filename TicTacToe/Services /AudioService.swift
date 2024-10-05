970656f34e08e0b83aceb499870015c236afe8e0
Nikita Kornuk <kornyk62@gmail.com>
2024-10-05 18:29:43 +0200

    add save in ud settings

--- TicTacToe/Services /AudioService.swift
+++ TicTacToe/Services /AudioService.swift
@@ -11,43 +11,69 @@ import SwiftUI
 final class AudioService: ObservableObject {
     @Published var isPlaySound: Bool = false
     private var audioPlayer: AVAudioPlayer?
-    private var selectedMusic: Constants.Music = .classical
-
+    private var selectedMusic: Constants.Music? = nil
+    
     init() {
         NotificationCenter.default.addObserver(self, selector: #selector(handleMusicSettingsChanged(notification:)), name: .musicSettingsChanged, object: nil)
+        loadInitialSettings()
     }
     
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
+    
+    private func loadInitialSettings() {
+        let isOnMusic = UserDefaults.standard.bool(forKey: Constants.KeysUD.isOnMusic)
+        let classicMusic = UserDefaults.standard.bool(forKey: Constants.KeysUD.classicMusic)
+        let instrumentalMusic = UserDefaults.standard.bool(forKey: Constants.KeysUD.instrumentalMusic)
+        let natureMusic = UserDefaults.standard.bool(forKey: Constants.KeysUD.natureMusic)
 
+        self.isPlaySound = isOnMusic
+        
+        if classicMusic {
+            selectedMusic = .classical
+        } else if instrumentalMusic {
+            selectedMusic = .instrumentals
+        } else if natureMusic {
+            selectedMusic = .nature
+        }
+
+        if isPlaySound, let music = selectedMusic {
+            playSound(soundFileName: music.rawValue)
+        }
+    }
+    
     @objc private func handleMusicSettingsChanged(notification: Notification) {
         guard let userInfo = notification.userInfo else { return }
         
         if let isOnMusic = userInfo["isOnMusic"] as? Bool {
             self.isPlaySound = isOnMusic
-            isOnMusic ? playSound(soundFileName: selectedMusic.rawValue) : stop()
+            if isOnMusic, let music = selectedMusic {
+                playSound(soundFileName: music.rawValue)
+            } else {
+                stop()
+            }
         }
         
         if let classicMusic = userInfo["classicMusic"] as? Bool, classicMusic {
             selectedMusic = .classical
-            if isPlaySound { playSound(soundFileName: selectedMusic.rawValue) }
+            if isPlaySound { playSound(soundFileName: selectedMusic!.rawValue) }
         }
         
         if let instrumentalMusic = userInfo["instrumentalMusic"] as? Bool, instrumentalMusic {
             selectedMusic = .instrumentals
-            if isPlaySound { playSound(soundFileName: selectedMusic.rawValue) }
+            if isPlaySound { playSound(soundFileName: selectedMusic!.rawValue) }
         }
         
         if let natureMusic = userInfo["natureMusic"] as? Bool, natureMusic {
             selectedMusic = .nature
-            if isPlaySound { playSound(soundFileName: selectedMusic.rawValue) }
+            if isPlaySound { playSound(soundFileName: selectedMusic!.rawValue) }
         }
     }
     
     private func playSound(soundFileName: String) {
         guard let url = Bundle.main.url(forResource: soundFileName, withExtension: "mp3") else { return }
-
+        
         do {
             try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
             audioPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
@@ -57,7 +83,7 @@ final class AudioService: ObservableObject {
             print(error.localizedDescription)
         }
     }
-
+    
     func stop() {
         audioPlayer?.stop()
         audioPlayer?.currentTime = 0
